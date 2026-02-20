81779953cdc67a7efde2f27bea2c4a8ba72e97a4
Iosi Pratama <pratamaiosi@gmail.com>
2026-02-20 14:42:59 +0700

    Initial Commit

---
+++ Bitcoin Tracker/Bitcoin_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bitcoin_TrackerApp.swift
+//  Bitcoin Tracker
+//
+//  Created by Iosi Pratama on 20/02/26.
+//
+
+import SwiftUI
+
+@main
+struct Bitcoin_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
